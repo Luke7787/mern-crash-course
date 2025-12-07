6530dd05e562bb367be6c8ef7b7a4231f76d591e
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-06 16:15:40 -0800

    implemented GET /api/products endpoint in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
+app.get("/api/products", async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.log("error in fetching products:", error.message);
+        res.status(500).json({ success: false, message: "Server Error "});
+    }
+});
+
 app.post("/api/products", async(req, res) => {
     const product = req.body;
 
@@ -37,6 +47,7 @@ app.delete("/api/products/:id", async(req, res) =>{
       await Product.findByIdAndDelete(id);
       res.status(200).json({success: true, message: "Product deleted" });
     } catch (error) {
+      console.log("error in deleting product:", error.message);
       res.status(404).json({success: false, message: "Product not found"});
     }
 
