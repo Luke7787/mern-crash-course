8dd58049047d5aae0f1cf37f6bb3eeeb0bbe2dc9
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-07 16:23:18 -0800

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(express.json()); // allows us to accept JSON data in the req.body 
 
 app.get("/api/products", async(req, res) => {
     try {
