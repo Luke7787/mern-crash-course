d4bc1f4f5a5a08c2ee91de9a0e945a93a22fdbd4
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-07 20:31:13 -0800

    reformat product.model.js

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,25 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true
+      required: true,
     },
     price: {
       type: Number,
-      required: true
+      required: true,
     },
     image: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
