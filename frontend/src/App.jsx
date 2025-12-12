2d261a9067365fa724bd37c92d09597eebd6ab94
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-12 15:56:12 -0800

    pushed updated version of App.jsx in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import { Button } from "@chakra-ui/react";
+import { Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Box minH={"100vh"}>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/create" element={<CreatePage />}></Route>
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
