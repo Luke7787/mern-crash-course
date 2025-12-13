3595013088ec09a9beba940eb2c1a22c467a5fba
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-12 16:39:00 -0800

    installed chakra-ui package, react-icon, made great changes to the Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import { Button } from "@chakra-ui/react";
-import { Routes } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Box minH={"100vh"}>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/create" element={<CreatePage />}></Route>
