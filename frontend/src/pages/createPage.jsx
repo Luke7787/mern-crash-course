a3588685470ec1b53d6beb0e7ddeebf33d87267c
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-13 17:51:06 -0800

    started on implementing useState and hook for createPage for new products

--- frontend/src/pages/createPage.jsx
+++ frontend/src/pages/createPage.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Box, Container, useColorModeValue, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
 
-const createPage = () => {
-  return <div>createPage</div>;
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        ></Box>
+      </VStack>
+    </Container>
+  );
 };
 
-export default createPage;
+export default CreatePage;
