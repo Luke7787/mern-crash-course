bc69c02d64a29676a97c1763fa625a38f276f233
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-14 17:47:00 -0800

    updated CreatePage so now createProduct form is now visable

--- frontend/src/pages/createPage.jsx
+++ frontend/src/pages/createPage.jsx
@@ -1,4 +1,12 @@
-import { Box, Container, useColorModeValue, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  useColorModeValue,
+  VStack,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 
 const CreatePage = () => {
@@ -7,6 +15,11 @@ const CreatePage = () => {
     price: "",
     image: "",
   });
+
+  const handleAddProduct = () => {
+    console.log(newProduct);
+  };
+
   return (
     <Container maxW={"container.sm"}>
       <VStack spacing={8}>
@@ -20,7 +33,41 @@ const CreatePage = () => {
           p={6}
           rounded={"lg"}
           shadow={"md"}
-        ></Box>
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+
+            <Input
+              placeholder="Price"
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+
+            <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
       </VStack>
     </Container>
   );
