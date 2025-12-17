805c58a320ef148f8cfef7b27b0325668fab1bc1
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-16 16:33:17 -0800

    import useToast hook to display message fail or success status after production creation attempt

--- frontend/src/pages/createPage.jsx
+++ frontend/src/pages/createPage.jsx
@@ -6,6 +6,7 @@ import {
   VStack,
   Heading,
   Input,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useProductStore } from "../store/product";
@@ -16,13 +17,27 @@ const CreatePage = () => {
     price: "",
     image: "",
   });
+  const toast = useToast();
 
   const { createProduct } = useProductStore();
 
   const handleAddProduct = async () => {
     const { success, message } = await createProduct(newProduct);
-    console.log("Success:", success);
-    console.log("Message:", message);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        isClosable: true,
+      });
+    }
   };
 
   return (
