0bf7d1157759b784b5f60d16a374a6db5b07322e
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-18 16:55:34 -0800

    added useEffect() and called fetchProducts function in HomePage.jsx, added console log for testing

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
 
 const HomePage = () => {
+  const {fetchProducts, products} = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products);
+
   return (
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8}>
