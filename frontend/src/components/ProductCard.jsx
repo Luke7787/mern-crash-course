d05c7d2c904bffa80059ab89efc0fc17d1120e07
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-19 17:20:33 -0800

    worked on productcard and homepage.jsx

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import React from "react";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  const textColor = useColorModeValue("gray.600", "gray.200");
+  const bg = useColorModeValue("white", "gray.800");
   return (
-    <div>ProductCard</div>
-  )
-}
+    <Box
+      shadow="lg"
+      rounded="lg"
+      overflow="hidden"
+      transition="all 0.3s"
+      _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+      bg={bg}
+    >
+      <Image
+        src={product.image}
+        alt={product.name}
+        h={48}
+        w="full"
+        objectFit="cover"
+      ></Image>
 
-export default ProductCard
+      <Box p={4}>
+        <Heading as="h3" size="md" mb={2}>
+          {product.name}
+        </Heading>
+
+        <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
+          ${product.price}
+        </Text>
+
+        <HStack spacing={2}>
+          <IconButton icon={<EditIcon />} colorScheme="blue" />
+          <IconButton icon={<DeleteIcon />} />
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProductCard;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,9 +2,10 @@ import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useProductStore } from "../store/product";
+import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
-  const {fetchProducts, products} = useProductStore();
+  const { fetchProducts, products } = useProductStore();
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
@@ -27,16 +28,14 @@ const HomePage = () => {
           columns={{
             base: 1,
             md: 2,
-            lg: 3
+            lg: 3,
           }}
           spacing={10}
           width={"full"}
         >
-
           {products.map((product) => (
-            <ProductCard key ={product._id} product={product}/>
+            <ProductCard key={product._id} product={product} />
           ))}
-
         </SimpleGrid>
 
         <Text
