c7e8d4689b4401d9d4a011906db8944b69b6fcec
Luke Zhuang <Lukezhuang@yahoo.com>
2025-12-22 15:45:37 -0800

    updated code typo on line 24 for products.js

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -18,11 +18,9 @@ export const useProductStore = create((set) => ({
     set((state) => ({ products: [...state.products, data.data] }));
     return { success: true, message: "Product created successfully" };
   },
-  fetchProducts: async ()=>{
+  fetchProducts: async () => {
     const res = await fetch("/api/products");
     const data = await res.json();
-    set({prodcuts: data.data});
-  }
+    set({ products: data.data });
+  },
 }));
-
-
