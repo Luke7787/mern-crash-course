de8ff2ed1f5cc2e401acf9aa0565bb39f7a318a4
Luke Zhuang <Lukezhuang@yahoo.com>
2026-01-05 16:51:29 -0800

    updated createPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import CreatePage from "./pages/CreatePage.jsx";
+import CreatePage from "./pages/createPage.jsx";
 import Navbar from "./components/Navbar";
 
 function App() {
